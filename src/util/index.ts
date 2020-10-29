162d43fece882eb73d80dc84ef1ec573de52a0ad
leroli <leroli@tencent.com>
2020-10-30 01:57:45 +0800

    feat: 增加获取相对路径URL工具函数

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,3 +3,19 @@ import { createFromIconfontCN } from '@ant-design/icons'
 export const MyIcon = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/font_2124777_4yxiduyyxnd.js', // 在 iconfont.cn 上生成
 })
+
+/**
+ * 获取当前页面的相对路径
+ */
+export function getUrlRelativePath() {
+  const url = document.location.toString()
+  const arrUrl = url.split('//')
+
+  const start = arrUrl[1].indexOf('/')
+  let relUrl = arrUrl[1].substring(start) //stop省略，截取从start开始到结尾的所有字符
+
+  if (relUrl.indexOf('?') !== -1) {
+    relUrl = relUrl.split('?')[0]
+  }
+  return relUrl
+}
