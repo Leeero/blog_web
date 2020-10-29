26cfac550a32c776375e7588a4c0aa22c8d98869
leroli <leroli@tencent.com>
2020-10-29 16:38:36 +0800

    feat: 新增文章详情markdown解析

--- src/pages/ArticleDetails/index.tsx
+++ src/pages/ArticleDetails/index.tsx
@@ -2,7 +2,44 @@ import AuthorCard from '@/components/AuthorCard'
 import { CalendarOutlined, FireOutlined, FolderOpenOutlined, HomeOutlined } from '@ant-design/icons'
 import { Breadcrumb, Col, Row } from 'antd'
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 import './index.scss'
+
+let markdown =
+  '# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+  '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n' +
+  '`console.log(111)` \n\n' +
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n' +
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n' +
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n' +
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n' +
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n' +
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n' +
+  '``` var a=11; ```'
 
 export default function ArticleDetails() {
   return (
@@ -33,8 +70,9 @@ export default function ArticleDetails() {
             </span>
           </div>
           <div className="details_content-markdown">
-            在上一篇的内容中，介绍了webpack的核心概念以及常用的核心配置，并尝试着配置了一个项目脚手架。那么接下来，就进入飞升环节——性能优化和实现原理。
-            锦上添花-优化 使用分析工具
+            <div className="detailed-content">
+              <ReactMarkdown source={markdown} escapeHtml={false} />
+            </div>
           </div>
         </Col>
 
